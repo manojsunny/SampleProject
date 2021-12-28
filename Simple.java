bfb9edec2c8dbdbe6c50e44e34d816191d591a89
manojsunny <manojsunny18@gmail.com>
2021-12-28 21:52:27 +0530

    Third Commit

---
+++ Simple.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Simple {
+
+
+	public static void main(String[] args) {
+	
+		generateMessage();
+
+		add(45,5412);
+	
+	}
+
+
+	public static void generateMessage() {	
+
+		Date date = new Date();
+
+		System.out.println(date);	
+	}
+
+	public static void add(int a, int b) {	
+
+		System.out.println(a+b);
+	}
+}
