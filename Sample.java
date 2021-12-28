de58c9b1494e0d01ea6b7d354d3d43fb04997ae8
manojsunny <manojsunny18@gmail.com>
2021-12-28 21:32:23 +0530

    second commit

---
+++ Sample.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Sample {
+
+
+	public static void main(String[] args) {
+	
+		generateMessage();
+	
+	
+	}
+
+
+	public static generateMessage() {
+	
+		Date date = new Date();
+
+		System.out.println(date);
+	
+	}
+
+
+}
